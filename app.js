19d9f54c930df5edb2e4631ac036a7cdd6816717
giovannimazzi <gioelor@libero.it>
2026-02-27 18:04:34 +0100

    add first route

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3000;
 const apiUrl = `http://localhost:${port}`;
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server del mio blog",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listening on ${apiUrl}`);
 });
